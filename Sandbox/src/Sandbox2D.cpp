0323f69daaf2dc479cb04f98ebb94e540396ae85
jxwu <1463009704@qq.com>
2023-12-25 14:44:25 +0800

    Render Rotated Quads in Batch  by Using Transform

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -43,15 +43,18 @@ void Sandbox2D::OnUpdate(XEg::TimeStep ts)
 		XEg::RenderCommand::Clear();
 	}
 	{
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
 		XE_PROFILE_SCOPE("Render Draw");
 
 		XEg::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-		// XEg::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+		XEg::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		XEg::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		XEg::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		XEg::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 1.0f);
-		XEg::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 1.0f);
+		XEg::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		XEg::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 2.0f);
 
 		XEg::Renderer2D::EndScene();
 	}
