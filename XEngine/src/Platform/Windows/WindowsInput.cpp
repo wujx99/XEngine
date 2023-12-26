f9848d78977ace2b66f3b15c4fcd7997661dba70
jxwu <1463009704@qq.com>
2023-12-26 16:16:38 +0800

    Refactor Input To Clearer Singleton, Like Renderer2D

--- XEngine/src/Platform/Windows/WindowsInput.cpp
+++ XEngine/src/Platform/Windows/WindowsInput.cpp
@@ -1,5 +1,5 @@
 #include "xepch.h"
-#include "WindowsInput.h"
+#include "XEngine/Core/Input.h"
 
 #include "XEngine/Core/Application.h"
 #include <GLFW/glfw3.h>
@@ -8,21 +8,21 @@ namespace XEg
 {
 	
 
-	bool WindowsInput::IsKeyPressedImpl(KeyCode keycode)
+	bool Input::IsKeyPressed(KeyCode keycode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(keycode));
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+	bool Input::IsMouseButtonPressed(MouseCode button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(button));
 		return state == GLFW_PRESS ;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	std::pair<float, float> Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
@@ -30,15 +30,15 @@ namespace XEg
 		return {(float)xpos, (float)ypos};
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 		return x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 		return y;
 	}
 
--- XEngine/src/XEngine/Core/Input.h
+++ XEngine/src/XEngine/Core/Input.h
@@ -7,28 +7,11 @@ namespace XEg
 {
 	class Input
 	{
-	protected:
-		Input() = default;
 	public:
-		virtual ~Input() = default;
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-
-		inline static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }
-		inline static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-
-		static Scope<Input> Create();
-	protected:
-		virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-	private:
-		static Scope<Input> s_Instance;
+		static bool IsKeyPressed(KeyCode key);
+		static bool IsMouseButtonPressed(MouseCode button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
