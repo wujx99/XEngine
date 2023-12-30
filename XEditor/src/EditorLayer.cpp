65156c72f722b068768bae9ee20559e460b922f2
jxwu <1463009704@qq.com>
2023-12-30 20:40:03 +0800

    Added Properties Panel

--- XEditor/src/EditorLayer.cpp
+++ XEditor/src/EditorLayer.cpp
@@ -41,16 +41,16 @@ namespace XEg
 		class CameraController : public ScriptaleEntity
 		{
 		public:
-			void OnCreate()
+			virtual void OnCreate() override
 			{
 				auto& transform = GetComponent<TransformComponent>().Transform;
 				transform[3][0] = rand() % 10 - 5.f;
 			}
-			void OnDestroy()
+			virtual void OnDestroy() override
 			{
 
 			}
-			void OnUpdate(TimeStep ts)
+			virtual void OnUpdate(TimeStep ts) override
 			{
 				auto& transform = GetComponent<TransformComponent>().Transform;
 				float speed = 5.f;
--- XEditor/src/Panels/SceneHierarchyPanel.cpp
+++ XEditor/src/Panels/SceneHierarchyPanel.cpp
@@ -2,6 +2,8 @@
 
 #include <imgui/imgui.h>
 
+#include <glm/gtc/type_ptr.hpp>
+
 #include "XEngine/Scene/Components.h"
 
 namespace XEg
@@ -26,6 +28,15 @@ namespace XEg
 			Entity entity{ entityID, m_Context.get() };
 			DrawEntityNode(entity);
 			});
+
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
 		ImGui::End();
 	}
 
@@ -45,6 +56,31 @@ namespace XEg
 			bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
 			if (opened)
 				ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponnent>())
+		{
+			auto& tag = entity.GetComponent<TagComponnent>().Tag;
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), .1f);
+				ImGui::TreePop();
+			}
 		}
 	}
 
