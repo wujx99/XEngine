5ef82f99e7af7fdcbb8c66acaa75de9472b9539f
jxwu <1463009704@qq.com>
2024-01-08 22:28:34 +0800

    Added drag/drop to Content Browser for loading scenes

--- XEditor/src/EditorLayer.cpp
+++ XEditor/src/EditorLayer.cpp
@@ -14,6 +14,8 @@
 
 namespace XEg
 {
+	extern const std::filesystem::path g_AssetPath;
+
 	EditorLayer::EditorLayer()
 		:Layer("EditorLayer"), m_CameraController(1280.f / 720.f), m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
 	{
@@ -232,7 +234,7 @@ namespace XEg
 		if (m_HoveredEntity)
 			name = m_HoveredEntity.GetComponent<TagComponnent>().Tag;
 		ImGui::Text("Hovered Entity: %s", name.c_str());
-		
+
 
 		auto stats = Renderer2D::GetStats();
 		ImGui::Text("Renderer2D Stats:");
@@ -257,13 +259,22 @@ namespace XEg
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused && !m_ViewportHovered);
 
 		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-		
+
 		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
-			
+
 		//XE_CLIENT_WARN("Viewport Size: {0}, {1}", viewportPanelSize.x, viewportPanelSize.y);
 		uint64_t textureID = m_Framebuffer->GetColorAttachmentRendererID(0);
 		ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2(0, 1), ImVec2(1, 0));
 
+		if (ImGui::BeginDragDropTarget())
+		{
+			if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+			{
+				const wchar_t* path = (const wchar_t*)payload->Data;
+				OpenScene(std::filesystem::path(g_AssetPath) / path);
+			}
+			ImGui::EndDragDropTarget();
+		}
 		
 		// Gizmos
 		Entity selectedEntity = m_SceneHierarchyPanel.GetSelectedEntity();
@@ -398,12 +409,18 @@ namespace XEg
 		std::string filepath = FileDialogs::OpenFile("XEngine Scene(*.XE)\0*.XE\0");
 		if (!filepath.empty())
 		{
-			m_ActiveScene = CreateRef<Scene>();
-			m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-			m_SceneHierarchyPanel.SetContext(m_ActiveScene);
-			SceneSerializer serializer(m_ActiveScene);
-			serializer.Deserialize(filepath);
+			OpenScene(filepath);
 		}
+	}
+
+	void EditorLayer::OpenScene(const std::filesystem::path& path)
+	{
+		m_ActiveScene = CreateRef<Scene>();
+		m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+		m_SceneHierarchyPanel.SetContext(m_ActiveScene);
+
+		SceneSerializer serializer(m_ActiveScene);
+		serializer.Deserialize(path.string());
 	}
 
 	void EditorLayer::SaveSceneAs()
--- XEditor/src/EditorLayer.h
+++ XEditor/src/EditorLayer.h
@@ -24,6 +24,7 @@ namespace XEg
 
 		void NewScene();
 		void OpenScene();
+		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
 	private:
 		OrthographicCameraController m_CameraController;
