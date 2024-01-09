300e7652f56198fd47f9993b285baed6149541b9
jxwu <1463009704@qq.com>
2024-01-09 11:15:14 +0800

    Added play/stop button to editor

--- XEditor/src/EditorLayer.h
+++ XEditor/src/EditorLayer.h
@@ -26,6 +26,12 @@ namespace XEg
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
+
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
 	private:
 		OrthographicCameraController m_CameraController;
 
@@ -52,8 +58,18 @@ namespace XEg
 		glm::vec4 m_SquareColor = { .2f, .3f, .8f, 1.f };
 
 		int m_GizmoType = -1;
+
+		enum class SceneState
+		{
+			Edit = 0, Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+		
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Editor resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
