6af3d2829bc5c1203b3ceb1a924ced1f33e93f2c
jxwu <1463009704@qq.com>
2024-01-10 19:05:41 +0800

    CopyComponent and CopyComponentIfExists as a variadic template functions

--- XEngine/src/XEngine/Scene/Components.h
+++ XEngine/src/XEngine/Scene/Components.h
@@ -152,4 +152,14 @@ namespace XEg
 		CircleCollider2DComponent() = default;
 		CircleCollider2DComponent(const CircleCollider2DComponent&) = default;
 	};
+
+	template<typename... Component>
+	struct ComponentGroup
+	{
+
+	};
+
+	using AllComponents = ComponentGroup < TransformComponent, SpriteRendererComponent,
+		CircleRendererComponent, CameraComponent, NativeScriptComponent,
+		Rigidbody2DComponent, BoxCollider2DComponent, CircleCollider2DComponent>;
 }
