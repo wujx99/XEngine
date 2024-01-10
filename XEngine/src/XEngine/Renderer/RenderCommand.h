82b162376ad43f731870b6cf92dfd53fc79e9e9d
jxwu <1463009704@qq.com>
2024-01-10 13:53:29 +0800

    Added line and rectangle rendering to Renderer2D

--- XEngine/src/XEngine/Renderer/RenderCommand.h
+++ XEngine/src/XEngine/Renderer/RenderCommand.h
@@ -7,11 +7,13 @@ namespace XEg
 	class RenderCommand
 	{
 	public:
-		inline static void Init() { s_RendererAPI->Init(); }
-		inline static void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) { s_RendererAPI->SetViewport(x, y, width, height); }
-		inline static void SetClearColor(const glm::vec4& color) { s_RendererAPI->SetClearColor(color); }
-		inline static void Clear() { s_RendererAPI->Clear(); }
-		inline static void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t count = 0) { s_RendererAPI->DrawIndexed(vertexArray, count); }
+		static void Init() { s_RendererAPI->Init(); }
+		static void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) { s_RendererAPI->SetViewport(x, y, width, height); }
+		static void SetClearColor(const glm::vec4& color) { s_RendererAPI->SetClearColor(color); }
+		static void Clear() { s_RendererAPI->Clear(); }
+		static void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) { s_RendererAPI->DrawIndexed(vertexArray, indexCount); }
+		static void DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount) { s_RendererAPI->DrawLines(vertexArray, vertexCount); }
+		static void SetLineWidth(float width) { s_RendererAPI->SetLineWidth(width); }
 	private:
 		static Scope<RendererAPI> s_RendererAPI;
 
